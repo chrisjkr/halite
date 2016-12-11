10433925dedaa3c931b4abe6fee66c84b8eaf7b9
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-11 22:47:53 +0100

    v4: scanning for weakest neighbours nearby and sending close squares to help, wins consistently with v3

--- v4.py
+++ v4.py
@@ -3,11 +3,11 @@ from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
 import logging as l
 
-l.basicConfig(filename='v3.log', level=l.DEBUG)
+l.basicConfig(filename='v4.log', level=l.DEBUG)
 l.disabled = False
 
 myID, game_map = hlt.get_init()
-hlt.send_init("krszwsk v3")
+hlt.send_init("krszwsk v4")
 
 def dirToStr(d):
     if d == STILL:
@@ -20,6 +20,9 @@ def dirToStr(d):
         return 'SOUTH'
     elif d == WEST:
         return 'WEST'
+
+def get_nice_string(list_or_iterator):
+    return "[" + ", ".join( str(x) for x in list_or_iterator) + "]"
 
 def find_closest_border(square):
     l.info('findClosestBorder called width location ' + str(square.x) + ', ' + str(square.y) + '.')
@@ -38,9 +41,25 @@ def find_closest_border(square):
     l.info('Closest border is ' + dirToStr(direction) + ' with distance ' + str(distance))
     return direction
 
+def find_path_to_location(home, target):
+    if home.x == target.x and home.y == target.y:
+        return STILL
+    if abs(home.x - target.x) >= abs(home.y - target.y):
+        if home.x > target.x:
+            return WEST
+        else:
+            return EAST
+    else:
+        if home.y > target.y:
+            return NORTH
+        else:
+            return SOUTH
+
 def move(square):
     border = False
+
     # Check if there's a weaker enemy neighbour
+    weakest_neighbour_strength = 255
     for d, neighbour in enumerate(game_map.neighbours(square)):
         # Check if neighbour is enemy
         if neighbour.owner != myID:
@@ -48,13 +67,25 @@ def move(square):
             # Fight if enemy is weaker
             if neighbour.strength < square.strength:
                 return Move(square, d)
+            else:
+                if neighbour.strength < weakest_neighbour_strength:
+                    weakest_neighbour_strength = neighbour.strength
 
-    if border or square.strength < square.production * 5:
+    if square.strength > square.production * 5:
+        for weakest in come_here:
+            if game_map.get_distance(square, weakest[0]) < 2 and weakest[1] < weakest_neighbour_strength:
+                return Move(square, find_path_to_location(square, weakest[0]))
+
+    come_here.append([square, weakest_neighbour_strength])
+
+    if border or square.strength < square.production * 4:
         return Move(square, STILL)
 
     return Move(square, find_closest_border(square))
 
 while True:
     game_map.get_frame()
+    come_here = []
     moves = [move(square) for square in game_map if square.owner == myID]
+    l.info(get_nice_string(come_here))
     hlt.send_frame(moves)
